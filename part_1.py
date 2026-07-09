0a33cc28def2d0b6c41ebdc5dd56e0b97516f511
ayushbamaniya04-source <ayushbamaniya04@gmail.com>
2026-07-10 02:31:57 +0530

    feat: add initial Field class interface.

---
+++ part_1.py
@@ -0,0 +1,19 @@
+class Field:
+
+    def __init__(self):
+        pass
+
+    def create(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def get_cell(self, x, y):
+        pass
+
+    def update_cell(self, cell, state):
+        pass
+
+    def reset(self):
+        pass
