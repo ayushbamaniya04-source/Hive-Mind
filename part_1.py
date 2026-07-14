5bde2db1252f1937e09369b43f2b98eb234ab017
ayushbamaniya04-source <ayushbamaniya04@gmail.com>
2026-07-15 02:00:15 +0530

    feat: implement field cell storage and lookup

--- part_1.py
+++ part_1.py
@@ -5,25 +5,28 @@ class Field:
         self.rows = []
         
     def create(self):
-            for row in range(5):
-                for col in range(20):
-                    cell = Cell(row, col)
-                    self.cells.append(cell)
+        for row in range(5):
+            for col in range(20):
+                cell = Cell(row, col)
+                self.cells.append(cell)
                     
     def draw(self):
         pass
 
-    def get_cell(self, x, y):
-        pass
+    def get_cell(self, row, col):
+        for cell in self.cells:
+            if cell.row == row and cell.col == col:
+                return cell
+        return None
 
     def update_cell(self, cell, state):
-        pass
+        cell.state = state
+        
 
     def reset(self):
         pass
     
 class Cell:
-        
         
     def __init__(self, row, col):
         self.row = row
@@ -35,3 +38,14 @@ field = Field()
 field.create()
 
 print(len(field.cells))
+crop = field.get_cell(2, 5)
+
+print(crop.row)
+print(crop.col)
+print(crop.state)
+field.update_cell(crop, "problem")
+
+print(crop.state)
+another_crop = field.get_cell(1, 3)
+
+print(another_crop.state)
